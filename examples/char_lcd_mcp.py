d7be395e5bfb6562deb4d7a6974db0b89fa123a4
Tony DiCola <tony@tonydicola.com>
2014-07-21 21:15:28 +0000

    Adjust MCP example to use RGB LCD.

--- examples/char_lcd_mcp.py
+++ examples/char_lcd_mcp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Example using a character LCD connected to an MCP23017 GPIO extender.
+# Example using an RGB character LCD connected to an MCP23017 GPIO extender.
 import math
 import time
 
@@ -14,9 +14,9 @@ lcd_d4        = 2
 lcd_d5        = 3
 lcd_d6        = 4
 lcd_d7        = 5
-lcd_backlight = 6
-# Optionally if the backlight is not controllable then set:
-# lcd_backlight = None
+lcd_red       = 6
+lcd_green     = 7
+lcd_blue      = 8
 
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
@@ -33,8 +33,8 @@ gpio = MCP.MCP23017()
 # gpio = MCP.MCP23017(0x24, busnum=1)
 
 # Initialize the LCD using the pins 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, 
-							lcd_columns, lcd_rows, lcd_backlight, gpio=gpio)
+lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, 
+							lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue, gpio=gpio)
 
 # Print a two line message
 lcd.message('Hello\nworld!')
